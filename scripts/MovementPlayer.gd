423abbb4481d350da93672e9aeb7882b87605b6f
Lacosabuena <114421347+nicolasg32@users.noreply.github.com>
2025-03-28 13:26:58 -0300

    init

---
+++ scripts/MovementPlayer.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+
+var speed = 300
+var acceleration = 2000
+var player_alive = true
+
+
+func _physics_process(delta:float) -> void:
+	print(velocity)
+	# Gravity 
+
+	
+		# Movement Input	
+	var move_input = Input.get_axis("move_left","move_right")
+	print(move_input)
+	velocity.x = move_toward(velocity.x,move_input*speed,acceleration*delta)
+		# Animation Movement
+
+	move_and_slide()
