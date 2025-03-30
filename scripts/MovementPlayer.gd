7c858aeafd8c5bce53947c7f58b52e4dede42784
Lacosabuena <114421347+nicolasg32@users.noreply.github.com>
2025-03-30 17:17:26 -0300

    background & movement

--- scripts/MovementPlayer.gd
+++ scripts/MovementPlayer.gd
@@ -1,21 +1,18 @@
 extends CharacterBody2D
 
-
-
 var speed = 300
 var acceleration = 2000
 var player_alive = true
 
-
-func _physics_process(delta:float) -> void:
+func _physics_process(delta: float) -> void:
 	print(velocity)
-	# Gravity 
-
 	
-		# Movement Input	
-	var move_input = Input.get_axis("moved_left","move_right")
+	# Movimiento
+	var move_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	print(move_input)
-	velocity.x = move_toward(velocity.x,move_input*speed,acceleration*delta)
-		# Animation Movement
+	
+	velocity.x = move_toward(velocity.x, move_input.x * speed, acceleration * delta)
+	velocity.y = move_toward(velocity.y, move_input.y * speed, acceleration * delta)
 
+	# Movimiento físico
 	move_and_slide()
