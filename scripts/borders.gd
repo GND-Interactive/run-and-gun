2d4f8e1b47caa946d03908e9f6a0108e8cc6357a
gonzalo <gonzalo.javier.cartes@gmail.com>
2025-04-04 13:27:07 -0300

    feat: Añadir limites de una escena
    -Se crea carpeta levels y una escena leve-1
    
    -Se añaden bordes a la escena con el nodo colissionshape
    
    -Se agrega script a los borders para mover el limite derecho

---
+++ scripts/borders.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+#Este Script maneja los limites de cada nivel
+@onready var enemies: Node2D = $"../Enemies"
+@onready var right_limit: StaticBody2D = $"right-limit"
+@onready var timer: Timer = $"../Timer"
+
+
+var CHANGE_STAGE_OFFSET: float = 500
+
+# Esta funcion es solo para probar la funcionalidad de mover el limite derecho
+func _ready():
+	await get_tree().create_timer(5.0).timeout
+	remove_all_enemies()
+	move_right_limit()
+
+#Elimina todos los enemigos
+func remove_all_enemies():
+	for enemy in enemies.get_children():
+		enemy.queue_free()
+		
+# Mueve el limite derecho a la siguiente stage
+# Genera un desplazamiento a la derecha del limite derecho
+func move_right_limit() -> void:
+	right_limit.position.x += CHANGE_STAGE_OFFSET
+	return
+
+func get_alive_enemies_count() -> int:
+	var count := 0
+	for enemy in enemies.get_children():
+		if enemy.is_inside_tree():  # asegúrate de que aún existe
+			count += 1
+	return count
+	
+
+func _process(delta: float) -> void:
+	print("Enemigos vivos: ", get_alive_enemies_count())
