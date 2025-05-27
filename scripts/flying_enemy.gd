1d09cd77dc20a993ff96190e4ca74c1efcd7a234
gonzalo <gonzalo.javier.cartes@gmail.com>
2025-05-27 16:57:30 -0400

    feat: Metodo settear colores de la wave
    - Metodo set_wave_colors recibe un arreglo de strings de tamaño 3 el cual settear los colores de los enemigos voladores

--- scripts/flying_enemy.gd
+++ scripts/flying_enemy.gd
@@ -10,16 +10,25 @@ var debilidad : Color
 var debilidades = {
 	"red" : Color(1,0,0),
 	"blue" : Color(0,0,1),
-	"purple": Color(1,1,0),
+	"purple": Color(1,0,1),
 }
+
 
 func _ready() -> void:
 	print("Debilidad del enemigo: ", debilidad)
 	animation_player.play("Fly")
-	set_color()
 
 ## Método para setear el color usando un string
 ## Los colores disponibles son "red", "blue" y "purple"
 ## Si no se especifica el color, se pinta de color "purple"
 func set_color(color_name: String = "purple") -> void:
-	sprite.modulate = color_name
+	if debilidades.has(color_name):
+		debilidad = debilidades[color_name]
+	else:
+		debilidad = debilidades["purple"]  # Valor por defecto
+	sprite.modulate = debilidad
+	print("Seteado el color ", debilidad, " como debilidad")
+
+
+func _procces(delta: float) -> void:
+	sprite.modulate = debilidad
--- scripts/levels/level-1/enemy_wave_1.gd
+++ scripts/levels/level-1/enemy_wave_1.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var Enemies: Node2D = $Path2D/PathFollow2D/Node2D
 
 ## Esta variable almacena las debilidades de los enemigos
 var debilidades : Array[String] 
@@ -9,7 +10,14 @@ var debilidades : Array[String]
 # Le pasamos
 func _ready() -> void:
 	animation_player.play("Move_enemies")
+	set_wave_colors(["red","purple","blue"])
+	
 
-
-func set_wave_colors(colors: Array[String]) -> void:
-	pass
+func set_wave_colors(colores: Array[String]) -> void:
+	debilidades = colores
+	# El enemigo actual al que le estamos agregando un color
+	var i = 0
+	for flyingEnemy in Enemies.get_children():
+		# Obtenemos el sprite para cambiarle el color
+		flyingEnemy.set_color(debilidades[i])
+		i+=1
